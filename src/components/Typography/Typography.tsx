6aaa031b845783db5f77f8ba88613e6e3e7b5618
Francisco José Villar Rosa <francisco.villar.rosa@tecsisa.com>
2022-08-12 17:33:18 +0200

    feat(Typography): add color as a prop and color definition

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 import { FC } from "react";
 
-type Variant = "title" | "section" | "caption";
+type Variant = "title" | "section" | "caption" | "button";
 
 type VariantStyle = {
   fontSize: number;
@@ -18,20 +18,37 @@ const stylesByVariant: Record<Variant, VariantStyle> = {
   title: { fontSize: 14, fontWeight: 600 },
   section: { fontSize: 22, fontWeight: 800 },
   caption: { fontSize: 10 },
+  button: { fontSize: 12, fontWeight: 700 },
+};
+
+type Color = "primary" | "secondary" | "delete";
+
+export const colors: Record<Color, string> = {
+  primary: "rgb(0,0,0)",
+  secondary: "rgb(255,255,255)",
+  delete: "rgb(89, 84, 80)",
 };
 
 interface TypographyProps {
   variant: Variant;
   children: string;
+  color?: Color;
 }
 
-const StyledDiv = styled("div")<TypographyProps>(({ variant }) => ({
+const StyledDiv = styled("div")<TypographyProps>(({ variant, color }) => ({
   ...stylesByVariant[variant],
   ...commonStyles,
+  color: color && colors[color],
 }));
 
-const Typography: FC<TypographyProps> = ({ children, variant }) => (
-  <StyledDiv variant={variant}>{children}</StyledDiv>
+const Typography: FC<TypographyProps> = ({
+  children,
+  variant,
+  color = "primary",
+}) => (
+  <StyledDiv variant={variant} color={color}>
+    {children}
+  </StyledDiv>
 );
 
 export default Typography;
